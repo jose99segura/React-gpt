060f2f3de8e320435e6108dd6c201b2f4232e357
jose99segura <jose99segura@gmail.com>
2024-05-25 11:47:38 +0200

    showing the image in our chat and image variation

--- src/presentation/pages/image-generation/ImageGenerationPage.tsx
+++ src/presentation/pages/image-generation/ImageGenerationPage.tsx
@@ -1,8 +1,99 @@
+import { useState } from "react"
+import { GptMessage, MyMessage, TypingLoader, TextMessageBox, GptMessageImage } from "../../components";
+import { imageGenerationUseCase } from "../../../core/use-cases";
 
-
+interface Message {
+  text: string;
+  isGpt: boolean;
+  info?: {
+    imageUrl: string;
+    alt: string;
+  }
+}
 
 export const ImageGenerationPage = () => {
+
+  // Estados
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async( text: string ) => {
+
+    setIsLoading(true);
+    setMessages( (prev) => [...prev, { text, isGpt: false }] );
+
+    const imageInfo = await imageGenerationUseCase( text );
+    setIsLoading(false);
+
+    if( !imageInfo) {
+      return setMessages( (prev) => [...prev, { text: "Error al generar la imagen", isGpt: true }] );
+    }
+
+    setMessages( (prev) => [
+      ...prev, 
+      { 
+        text: text, 
+        isGpt: true, 
+        info: {
+          imageUrl: imageInfo.url,
+          alt: imageInfo.alt
+        }
+      }
+    ]);
+
+
+  }
+
+
   return (
-    <div>ImageGenerationPage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+
+          {/* Mensaje de GPT */}
+          <GptMessage text="Hola, puedes escribir tu texto en español, y generaré una imagen a partir del texto." />
+
+          { 
+            messages.map( (message, index) => (
+
+              message.isGpt 
+              ? (
+                // Mensaje de OpenAI
+                <GptMessageImage 
+                  key={index} 
+                  text={message.text}
+                  imageUrl={message.info?.imageUrl!}
+                  alt={message.info?.alt!} 
+                />
+              )
+              : (
+                // Mensaje de usuario
+                <MyMessage key={index} text={message.text} />
+              )
+
+            ))
+          }
+
+          {/* Loader */}
+          {
+            isLoading && (
+              <div className="col-start-1 col-end-12 fade-in">
+                <TypingLoader/>
+              </div>
+            )
+          }
+         
+          
+        </div>
+      </div>
+
+      {/* Input de texto */}
+      <TextMessageBox
+        onSendMessage={ handlePost }
+        placeholder="Escribe un mensaje..."
+        disableCorrections
+      />
+
+    </div>
   )
 }
--- src/presentation/pages/image-generation/ImageTunningPage.tsx
+++ src/presentation/pages/image-generation/ImageTunningPage.tsx
@@ -1,8 +1,141 @@
 
+import { useState } from "react"
+import { GptMessage, MyMessage, TypingLoader, TextMessageBox, GptMessageImage } from "../../components";
+import { imageGenerationUseCase, imageVariationUseCase } from "../../../core/use-cases";
 
+interface Message {
+  text: string;
+  isGpt: boolean;
+  info?: {
+    imageUrl: string;
+    alt: string;
+  }
+}
 
 export const ImageTunningPage = () => {
+
+  // Estados
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [originalImageAndMask, setOriginalImageAndMask] = useState({
+    original: 'http://localhost:3000/gpt/image-generation/1716630012740.png' as string | undefined,
+    mask: undefined as string | undefined,
+  })
+
+  const handleVariation = async() => {
+    setIsLoading(true);
+    const resp = await imageVariationUseCase( originalImageAndMask.original! );
+    setIsLoading(false);
+
+    if ( !resp )return;
+
+    setMessages( (prev) => [
+      ...prev,
+      {
+        text: 'Variación',
+        isGpt: true,
+        info: {
+          imageUrl: resp.url,
+          alt: resp.alt
+        }
+      }
+    ])
+
+  }
+
+  const handlePost = async( text: string ) => {
+
+    setIsLoading(true);
+    setMessages( (prev) => [...prev, { text, isGpt: false }] );
+
+    const imageInfo = await imageGenerationUseCase( text );
+    setIsLoading(false);
+
+    if( !imageInfo) {
+      return setMessages( (prev) => [...prev, { text: "Error al generar la imagen", isGpt: true }] );
+    }
+
+    setMessages( (prev) => [
+      ...prev, 
+      { 
+        text: text, 
+        isGpt: true, 
+        info: {
+          imageUrl: imageInfo.url,
+          alt: imageInfo.alt
+        }
+      }
+    ]);
+
+
+  }
+
+
   return (
-    <div>ImageTunningPage</div>
+    <>
+    {
+      originalImageAndMask.original && (
+        <div className="fixed flex flex-col items-center top-10 right-10 z-10 fade-in">
+          <span>Editando</span>
+          <img 
+            className="border rounded-xl w-36 h-36 object-contain"
+            src={ originalImageAndMask.mask ?? originalImageAndMask.original } 
+            alt="Imagen original"
+          />
+          <button onClick={ handleVariation } className="btn-primary mt-2">Generar variación</button>
+        </div>
+      )
+    }
+     <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+
+          {/* Mensaje de GPT */}
+          <GptMessage text="Hola, puedes escribir tu texto en español, y generaré una imagen a partir del texto." />
+
+          { 
+            messages.map( (message, index) => (
+
+              message.isGpt 
+              ? (
+                // Mensaje de OpenAI
+                <GptMessageImage 
+                  key={index} 
+                  text={message.text}
+                  imageUrl={message.info?.imageUrl!}
+                  alt={message.info?.alt!} 
+                />
+              )
+              : (
+                // Mensaje de usuario
+                <MyMessage key={index} text={message.text} />
+              )
+
+            ))
+          }
+
+          {/* Loader */}
+          {
+            isLoading && (
+              <div className="col-start-1 col-end-12 fade-in">
+                <TypingLoader/>
+              </div>
+            )
+          }
+         
+          
+        </div>
+      </div>
+
+      {/* Input de texto */}
+      <TextMessageBox
+        onSendMessage={ handlePost }
+        placeholder="Escribe un mensaje..."
+        disableCorrections
+      />
+
+    </div>
+    </>
+   
   )
 }
